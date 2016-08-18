5b9143f799eb947427ff890dc6c9eb3284d93c70
Jerry Chen <evil3378@gmail.com>
2016-08-18 16:11:28 +0800

    JerryChen - ADD Android Project Template Signed-off-by: Jerry Chen <evil3378@gmail.com>

---
+++ TravelMap/app/src/main/java/personalproject/evace/travelmap/MainActivity.java
@@ -0,0 +1,13 @@
+package personalproject.evace.travelmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
